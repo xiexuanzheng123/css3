fdab83eb36d997b4080e56a5edf3b5f18a5517f2
xiexuanzheng123 <349328984@qq.com>
2018-01-30 18:41:41 +0800

    compare the four methods of applying css

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
             hash: true,
             chunks: ['index'],
             filename: 'index.html',
-            template: './index.html',
+            template: './src/html/index.html',
             favicon: './src/images/css3.jpg'
         })
     ],
