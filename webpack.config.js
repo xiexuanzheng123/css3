a4d2cd9942209973f0389ddb1940c85dea095188
xiexuanzheng123 <349328984@qq.com>
2018-01-30 17:32:03 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const webpack = require('webpack');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: {
+        index: [
+            'babel-polyfill',
+            'webpack-hot-middleware/client?reload=true',
+            './index.js'
+        ]
+    },
+    output: {
+        filename: '[name].bundle.js',
+        publicPath: '/dist/',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|browser_components)/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'images/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'css3',
+            hash: true,
+            chunks: ['index'],
+            filename: 'index.html',
+            template: './index.html',
+            favicon: './src/images/css3.jpg'
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.json']
+    } 
+}
