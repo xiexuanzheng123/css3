771c1fdad113a11a08ac89b4e32e178302e3d79f
xiexuanzheng123 <349328984@qq.com>
2018-02-02 18:06:43 +0800

    relative

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('it works!');
+import _ from 'lodash';
+let arr = [2, 3, 4, 1, 23, 123];
+
+console.log(_.sortBy(arr));
--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
             hash: true,
             chunks: ['index'],
             filename: 'index.html',
-            template: './src/html/float.html',
+            template: './src/html/position.html',
             favicon: './src/images/css3.jpg'
         })
     ],
