790d296f6d05c25f3f4d3aa7c8920fc7d585b9f2
xiexuanzheng123 <349328984@qq.com>
2018-02-05 10:20:49 +0800

    position static

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
             hash: true,
             chunks: ['index'],
             filename: 'index.html',
-            template: './src/html/position.html',
+            template: './src/html/static.html',
             favicon: './src/images/css3.jpg'
         })
     ],
