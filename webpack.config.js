3837575307204cb67dbe5e20db27f969de08eb6e
xiexuanzheng123 <349328984@qq.com>
2018-02-05 10:31:09 +0800

    position fixed

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
             hash: true,
             chunks: ['index'],
             filename: 'index.html',
-            template: './src/html/static.html',
+            template: './src/html/fixed.html',
             favicon: './src/images/css3.jpg'
         })
     ],
