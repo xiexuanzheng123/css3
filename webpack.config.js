a80f0068b81f54a1ea8a562a744fb5008994f3c6
xiexuanzheng123 <349328984@qq.com>
2018-02-05 11:20:07 +0800

    position absolute

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
             hash: true,
             chunks: ['index'],
             filename: 'index.html',
-            template: './src/html/relative.html',
+            template: './src/html/absolute.html',
             favicon: './src/images/css3.jpg'
         })
     ],
