9f11c41adce66dd76c95da41363b11e0d0880c3a
xiexuanzheng123 <349328984@qq.com>
2018-02-08 10:41:59 +0800

    sticky

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
             hash: true,
             chunks: ['index'],
             filename: 'index.html',
-            template: './src/html/z-index2.html',
+            template: './src/html/sticky.html',
             favicon: './src/images/css3.jpg'
         })
     ],
