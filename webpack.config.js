591c5f06b620adfe154998316d1cc1e55f822edc
xiexuanzheng123 <349328984@qq.com>
2018-02-11 16:03:49 +0800

    vertical align

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
             hash: true,
             chunks: ['index'],
             filename: 'index.html',
-            template: './src/selector/variant.html',
+            template: './src/selector/align.html',
             favicon: './src/images/css3.jpg'
         })
     ],
