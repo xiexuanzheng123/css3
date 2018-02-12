97e0874d3f445da9f158fdc6276bf7318bb99559
xiexuanzheng123 <349328984@qq.com>
2018-02-12 11:38:28 +0800

    background

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
             hash: true,
             chunks: ['index'],
             filename: 'index.html',
-            template: './src/text/line-height.html',
+            template: './src/background/image.html',
             favicon: './src/images/css3.jpg'
         })
     ],
