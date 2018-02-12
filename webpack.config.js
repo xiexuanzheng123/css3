02a3f9beb47c8841685443a770bc987a677a10c9
xiexuanzheng123 <349328984@qq.com>
2018-02-12 10:14:40 +0800

    line height

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
             hash: true,
             chunks: ['index'],
             filename: 'index.html',
-            template: './src/text/white-spacing.html',
+            template: './src/text/line-height.html',
             favicon: './src/images/css3.jpg'
         })
     ],
