78a23994a100558e5cfecbd51e8f1f2cc6dcd96c
xiexuanzheng123 <349328984@qq.com>
2018-02-26 14:27:58 +0800

    vertical-line

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
             hash: true,
             chunks: ['index'],
             filename: 'index.html',
-            template: './src/nav/row.html',
+            template: './src/nav/vertical-line.html',
             favicon: './src/images/css3.jpg'
         })
     ],
