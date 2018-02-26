b9c2fd87aa846421116172c23f37bf050f68e5b2
xiexuanzheng123 <349328984@qq.com>
2018-02-26 10:52:19 +0800

    row navigator

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
             hash: true,
             chunks: ['index'],
             filename: 'index.html',
-            template: './src/nav/vertical.html',
+            template: './src/nav/row.html',
             favicon: './src/images/css3.jpg'
         })
     ],
