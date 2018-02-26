9b43fa7074c019886ba442d35005cc4bfa621465
xiexuanzheng123 <349328984@qq.com>
2018-02-26 14:41:48 +0800

    add border to list

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
             hash: true,
             chunks: ['index'],
             filename: 'index.html',
-            template: './src/nav/vertical-line.html',
+            template: './src/nav/triangle.html',
             favicon: './src/images/css3.jpg'
         })
     ],
